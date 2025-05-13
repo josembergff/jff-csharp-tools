4b61d2393033de1561a483d7bdcd17d55a3d919d
Josemberg Ferreira <josembergff@gmail.com>
2025-05-13 13:41:08 -0300

    Remover métodos obsoletos no DefaultRepository para simplificar a implementação e melhorar a manutenção do código.

--- jff-csharp-tools-6/Domain/Repository/DefaultRepository.cs
+++ jff-csharp-tools-6/Domain/Repository/DefaultRepository.cs
@@ -33,20 +33,6 @@ namespace JffCsharpTools6.Domain.Repository
             return entity.Id > 0;
         }
 
-        public async Task<bool> UpdateKey<TEntity, TKey>(TEntity entity, TKey key) where TEntity : DefaultEntity<TEntity>, new()
-        {
-            var existingEntity = await dbContext.Set<TEntity>().FindAsync(key);
-            if (existingEntity != null)
-            {
-                dbContext.Entry(existingEntity).CurrentValues.SetValues(entity);
-            }
-            else
-            {
-                dbContext.Update<TEntity>(entity);
-            }
-            return entity.Id > 0;
-        }
-
         public virtual async Task UpdateBatch<TEntity>(IEnumerable<TEntity> entityList, bool forceDetach = false, bool saveChanges = false) where TEntity : DefaultEntity<TEntity>, new()
         {
             if (forceDetach)
@@ -57,22 +43,6 @@ namespace JffCsharpTools6.Domain.Repository
                 }
             }
             dbContext.UpdateRange(entityList);
-            if (saveChanges)
-            {
-                await dbContext.SaveChangesAsync();
-            }
-        }
-
-        public virtual async Task UpdateInBatch<TEntity>(IEnumerable<TEntity> listEntity, bool forceDetach = false, bool saveChanges = false) where TEntity : DefaultEntity<TEntity>, new()
-        {
-            if (forceDetach)
-            {
-                foreach (var entity in listEntity)
-                {
-                    Detach(entity);
-                }
-            }
-            dbContext.UpdateRange(listEntity);
             if (saveChanges)
             {
                 await dbContext.SaveChangesAsync();
@@ -90,16 +60,6 @@ namespace JffCsharpTools6.Domain.Repository
         }
 
         public virtual async Task<IEnumerable<TEntity>> CreateBatch<TEntity>(IEnumerable<TEntity> entities, bool saveChanges = false) where TEntity : DefaultEntity<TEntity>, new()
-        {
-            await dbContext.Set<TEntity>().AddRangeAsync(entities);
-            if (saveChanges)
-            {
-                await dbContext.SaveChangesAsync();
-            }
-            return entities;
-        }
-
-        public virtual async Task<IEnumerable<TEntity>> CreateInBatch<TEntity>(IEnumerable<TEntity> entities, bool saveChanges = false) where TEntity : DefaultEntity<TEntity>, new()
         {
             await dbContext.Set<TEntity>().AddRangeAsync(entities);
             if (saveChanges)
@@ -146,21 +106,6 @@ namespace JffCsharpTools6.Domain.Repository
             return current;
         }
 
-        public async Task<TEntity> GetKey<TEntity, TKey>(TKey key, string[] include = null) where TEntity : DefaultEntity<TEntity>, new()
-        {
-            IQueryable<TEntity> query = dbContext.Set<TEntity>();
-
-            if (include?.Any() == true)
-                foreach (string lineInclue in include)
-                    query = query.Include(lineInclue);
-
-            var list = (DbSet<TEntity>)query;
-
-            var atual = await list.FindAsync(key);
-
-            return atual;
-        }
-
         public async Task<PaginationModel<TEntity>> GetPaginated<TEntity>(PaginationModel<TEntity> pagination, string[] include = null) where TEntity : DefaultEntity<TEntity>, new()
         {
             List<TEntity> listReturn = new List<TEntity>();
@@ -204,49 +149,6 @@ namespace JffCsharpTools6.Domain.Repository
             return pagination;
         }
 
-        public async Task<PaginationModel<TEntity>> GetPagination<TEntity>(PaginationModel<TEntity> paginacao, string[] include = null) where TEntity : DefaultEntity<TEntity>, new()
-        {
-            List<TEntity> listReturn = new List<TEntity>();
-
-            if (paginacao.Page > 0)
-            {
-                var query = dbContext.Set<TEntity>().Where(f => f.Id > 0);
-
-                if (include != null && include.Any())
-                    foreach (string lineInclue in include)
-                        query = query.Include(lineInclue);
-
-                paginacao.Total = query.Count();
-                if (paginacao.CountPage > 0)
-                {
-                    query = query.Skip(paginacao.CountPage * paginacao.Page).Take(paginacao.CountPage);
-                }
-
-                if (!string.IsNullOrEmpty(paginacao.Order))
-                {
-                    var paramRefer = char.ToUpper(paginacao.Order[0]) + paginacao.Order.Substring(1);
-                    var pi = typeof(TEntity).GetProperty(paramRefer);
-
-                    if (pi != null && !string.IsNullOrEmpty(paginacao.TypeOrder) && paginacao.TypeOrder.ToUpper() == "DESC")
-                    {
-                        query = query.OrderByDescending(x => pi.GetValue(x, null));
-                    }
-                    else if (pi != null)
-                    {
-                        query = query.OrderBy(x => pi.GetValue(x, null));
-                    }
-                }
-                listReturn = await query.ToListAsync();
-            }
-            else
-            {
-                listReturn = await dbContext.Set<TEntity>().Where(f => f.Id > 0).OrderByDescending(o => o.CreatedAt).ToListAsync();
-                paginacao.Total = listReturn.Count();
-            }
-            paginacao.List = listReturn;
-            return paginacao;
-        }
-
         public async Task<TEntity> GetFirstOrDefault<TEntity>(Expression<Func<TEntity, bool>> filter, string[] include = null) where TEntity : DefaultEntity<TEntity>, new()
         {
             IQueryable<TEntity> query = dbContext.Set<TEntity>();
@@ -274,20 +176,6 @@ namespace JffCsharpTools6.Domain.Repository
             return list;
         }
 
-        public async Task<IEnumerable<TEntity>> GetUser<TEntity>(int idUsuario, string[] include = null) where TEntity : DefaultEntity<TEntity>, new()
-        {
-            List<TEntity> lista = new List<TEntity>();
-            IQueryable<TEntity> query = dbContext.Set<TEntity>();
-
-            if (include?.Any() == true)
-                foreach (string lineInclude in include)
-                    query = query.Include(lineInclude);
-
-            lista = await query.Where(f => f.Id > 0).OrderByDescending(o => o.CreatedAt).ToListAsync();
-
-            return lista;
-        }
-
         public async Task<bool> Delete<TEntity>(Expression<Func<TEntity, bool>> filter, bool saveChanges = false) where TEntity : DefaultEntity<TEntity>, new()
         {
             var baseList = await dbContext.Set<TEntity>().Where(filter).ToListAsync();
@@ -311,31 +199,12 @@ namespace JffCsharpTools6.Domain.Repository
             }
         }
 
-        public virtual async Task DeleteInBatch<TEntity>(IEnumerable<TEntity> listEntity, bool saveChanges = false) where TEntity : DefaultEntity<TEntity>, new()
-        {
-            dbContext.RemoveRange(listEntity);
-            if (saveChanges)
-            {
-                await dbContext.SaveChangesAsync();
-            }
-        }
-
         public async Task<bool> DeleteByKey<TEntity, TKey>(TKey key) where TEntity : DefaultEntity<TEntity>, new()
         {
             var baseObj = await dbContext.Set<TEntity>().FindAsync(key);
             if (baseObj != null)
             {
                 dbContext.Set<TEntity>().Remove(baseObj);
-            }
-            return true;
-        }
-
-        public async Task<bool> DeleteKey<TEntity, TKey>(TKey key) where TEntity : DefaultEntity<TEntity>, new()
-        {
-            var objBase = await dbContext.Set<TEntity>().FindAsync(key);
-            if (objBase != null)
-            {
-                dbContext.Set<TEntity>().Remove(objBase);
             }
             return true;
         }
