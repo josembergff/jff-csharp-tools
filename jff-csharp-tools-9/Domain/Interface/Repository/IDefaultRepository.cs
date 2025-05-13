8f17a40b3d5a98dea0bd67661504bf393e0e4d6c
Josemberg Ferreira <josembergff@gmail.com>
2025-05-13 13:26:25 -0300

    Renomear métodos no repositório e serviço para melhorar a clareza e consistência, incluindo a adição de novos métodos para operações de atualização e filtragem.

--- jff-csharp-tools-9/Domain/Interface/Repository/IDefaultRepository.cs
+++ jff-csharp-tools-9/Domain/Interface/Repository/IDefaultRepository.cs
@@ -11,17 +11,17 @@ namespace JffCsharpTools9.Domain.Interface.Repository
     public interface IDefaultRepository<T> where T : DbContext
     {
         Task<TEntity> Create<TEntity>(TEntity entity, bool saveChanges = false) where TEntity : DefaultEntity<TEntity>, new();
-        Task<IEnumerable<TEntity>> CreateInBatch<TEntity>(IEnumerable<TEntity> entities, bool saveChanges = false) where TEntity : DefaultEntity<TEntity>, new();
-        Task<TEntity> GetKey<TEntity, TKey>(TKey key, string[] include = null) where TEntity : DefaultEntity<TEntity>, new();
+        Task<IEnumerable<TEntity>> CreateBatch<TEntity>(IEnumerable<TEntity> entities, bool saveChanges = false) where TEntity : DefaultEntity<TEntity>, new();
+        Task<TEntity> GetByKey<TEntity, TKey>(TKey key, string[] include = null) where TEntity : DefaultEntity<TEntity>, new();
         Task<TEntity> GetFirstOrDefault<TEntity>(Expression<Func<TEntity, bool>> filter, string[] include = null) where TEntity : DefaultEntity<TEntity>, new();
         Task<IEnumerable<TEntity>> Get<TEntity>(Expression<Func<TEntity, bool>> filter, string[] include = null, bool asNoTracking = false) where TEntity : DefaultEntity<TEntity>, new();
-        Task<IEnumerable<TEntity>> GetUser<TEntity>(int idUsuario, string[] include = null) where TEntity : DefaultEntity<TEntity>, new();
-        Task<PaginationModel<TEntity>> GetPagination<TEntity>(PaginationModel<TEntity> paginacao, string[] include = null) where TEntity : DefaultEntity<TEntity>, new();
-        Task<bool> UpdateKey<TEntity, TKey>(TEntity entidade, TKey key) where TEntity : DefaultEntity<TEntity>, new();
-        Task UpdateInBatch<TEntity>(IEnumerable<TEntity> listEntity, bool forceDetach = false, bool saveChanges = false) where TEntity : DefaultEntity<TEntity>, new();
+        Task<IEnumerable<TEntity>> GetByUser<TEntity>(int userId, string[] include = null) where TEntity : DefaultEntity<TEntity>, new();
+        Task<PaginationModel<TEntity>> GetPaginated<TEntity>(PaginationModel<TEntity> pagination, string[] include = null) where TEntity : DefaultEntity<TEntity>, new();
+        Task<bool> UpdateByKey<TEntity, TKey>(TEntity entity, TKey key) where TEntity : DefaultEntity<TEntity>, new();
+        Task UpdateBatch<TEntity>(IEnumerable<TEntity> entityList, bool forceDetach = false, bool saveChanges = false) where TEntity : DefaultEntity<TEntity>, new();
         Task<bool> Delete<TEntity>(Expression<Func<TEntity, bool>> filter, bool saveChanges = false) where TEntity : DefaultEntity<TEntity>, new();
-        Task DeleteInBatch<TEntity>(IEnumerable<TEntity> listEntity, bool saveChanges = false) where TEntity : DefaultEntity<TEntity>, new();
-        Task<bool> DeleteKey<TEntity, TKey>(TKey key) where TEntity : DefaultEntity<TEntity>, new();
+        Task DeleteBatch<TEntity>(IEnumerable<TEntity> entityList, bool saveChanges = false) where TEntity : DefaultEntity<TEntity>, new();
+        Task<bool> DeleteByKey<TEntity, TKey>(TKey key) where TEntity : DefaultEntity<TEntity>, new();
         Task SaveChangesAsync();
     }
 }
